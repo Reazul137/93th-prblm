0f536eda86c7dc54d792bf372691023022235bc5
Reazul Islam <reazulislam137@gmail.com>
2018-03-31 11:38:32 +0600

    first commit

---
+++ computerArea.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double radius;
+    double area;
+    radius = 20;
+
+    area = radius * radius * 3.1416;
+
+    cout<< "The area is "<<area<<endl;
+
+    return 0;
+}
+
